e22ce9f1ccf9384cb4656e811977bda7b7d18c16
ömer turan <126421999+mrTuranWebDeveloper@users.noreply.github.com>
2023-06-29 01:46:51 +0300

    Add files via upload

---
+++ react-booklist/src/books.js
@@ -0,0 +1,27 @@
+// import img1 from './images/book-1.jpg';
+// import img2 from './images/book-2.jpg';
+// import img3 from './images/book-3.jpg';
+
+const books = [
+    {
+        author : 'Rebecca Yarros',
+        title : 'Iron Flame',
+        img : './images/book-1.jpg',
+        id: 1,
+    },
+    
+    {
+        author : 'Abraham Verghese',
+        title : 'The Covenant of Water ',
+        img : './images/book-2.jpg',
+        id: 2,
+    },
+    {
+        author : 'Osamu Dazai',
+        title : 'İnsanlığımı Yitirirken',
+        img : './images/book-3.jpg',
+        id: 3,
+    },
+]
+
+export default books;
